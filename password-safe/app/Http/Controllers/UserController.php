d9400b01616cb42cdf7c7dc05ecac3d770b361c9
wesley <wesleyrodrigomoreira@gmail.com>
2023-08-19 11:47:58 -0300

    Padronizacao feita

---
+++ password-safe/app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::paginate();
+
+        return new UserResource($user);
+    }
+}
